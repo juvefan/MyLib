b0bac4de22ea3c47e9182502193cca8dc9c6d01c
juvefan <evg.and.juvefan@gmail.com>
2016-01-31 11:19:05 +0200

    new branch: display

--- restLib/src/anotherWorkspase/Work.java
+++ restLib/src/anotherWorkspase/Work.java
@@ -5,5 +5,7 @@ public class Work {
 	public static void main(String[] args) {
 		System.out.println("newe workspase");
 		System.out.println("newe workspase 2");
+		Display dis = new Display();
+		dis.display();
 	}
 }
