78efa8d80f87d30ad817d85f7feaa06db1045d56
juvefan <evg.and.juvefan@gmail.com>
2016-01-31 11:55:42 +0200

    change method display()

--- restLib/src/anotherWorkspase/Display.java
+++ restLib/src/anotherWorkspase/Display.java
@@ -3,7 +3,8 @@ package anotherWorkspase;
 public class Display {
 	public void display(){
 		for(int i = 0; i<5; i++){
-			System.out.println(i);
+			System.out.println(i+" ");
+			System.out.println(i*i);
 		}
 		
 	}
